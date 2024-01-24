9472b911336d722ca80fd491b42dde9891115ffe
Barrarroso <correodelnino@gmail.com>
2024-01-23 22:52:39 -0500

    Test landing page

---
+++ android-tutorial/astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Android Quickstart',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+      customCss: [
+        './src/styles/custom.css'
+      ],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
